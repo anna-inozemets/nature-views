ebd744d7688620590250f5342a76bceebab1e840
Anna Inozemets <inoznetta@gmail.com>
2023-03-31 16:34:18 +0300

    Footer was implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 export const App = () => {
   return (
     <main>
       <Header />
+      <Footer />
     </main>
   );
 };
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Footer.css';
+import instagram from '../../images/instagram.svg';
+import twitter from '../../images/twitter.svg';
+import facebook from '../../images/facebook.svg';
+
+export const Footer = () => {
+  return (
+    <footer>
+      <section className='section__fotter footer'>
+        <p className="footer__logo">
+          trppd __
+        </p>
+        <p className='footer__rights'>
+          &copy; 2017-2018
+        </p>
+        <div className="fotter__contacts">
+          <img 
+            className='footer__icon' 
+            src={instagram}
+            alt="instagram" 
+          />
+          <img 
+            className='footer__icon' 
+            src={twitter}
+            alt="twitter" 
+          />
+          <img 
+            className='footer__icon' 
+            src={facebook} 
+            alt="facebook" 
+          />
+        </div>
+      </section>
+    </footer>
+  );
+};
